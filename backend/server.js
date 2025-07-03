1cac025220e917ca04a5f84953b34a3c199b668a
Shadrack <shadrackmeshack7@gmail.com>
2025-07-03 20:07:22 +0300

    added front-end files

--- backend/server.js
+++ backend/server.js
@@ -15,4 +15,6 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
 
+console.log('MONGO_URI:', process.env.MONGO_URI);
+
 app.listen(process.env.PORT || 5000, () => console.log('Server running on port 5000'));
